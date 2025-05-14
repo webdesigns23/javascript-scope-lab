1a52c506a89e4293c105bb6fc214a4b8d240d41c
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-14 08:39:02 -0600

    Added Flatburger Restaurant Scope Code

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+//Global Scope:
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+//Function-Scoped:
+function addBurger() {
+    const newBurger = "Flatburger";  
+    burgers.push(newBurger);      
+
+    // Block-Scoped:
+if (true) {
+    const anotherNewBurger = "Maple Bacon Burger"; 
+    burgers.push(anotherNewBurger);             
+    return anotherNewBurger;   
+    }
+}
+
+//Global Scope:
+function changeFeatureDrink (){
+     featuredDrink = "The JavaShake";
+ }
+ 
+//Welcome Message- Global Scope
+console.log("Welcome to Flatburger");
+
+//Testing
+console.log(burgers);
+console.log(featuredDrink);
+
+
+
+
+
+
